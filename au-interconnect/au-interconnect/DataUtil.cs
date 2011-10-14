9672d449d2d307c66019948ec6df241cdb9945a6
Joseph Gay <gilleylen@gmail.com>
2011-10-14 17:41:43 -0500

    adding common utility for retrieving DbConnection using generic factory method (see #1)

---
+++ au-interconnect/au-interconnect/DataUtil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Web;
+
+namespace AUInterconnect
+{
+    public class DataUtil
+    {
+        public static DbConnection GetDbConnection()
+        {
+            // TODO: where to read connection name?
+            String connectionName = "findMe";
+
+            ConnectionStringSettings connStringSettings = ConfigurationManager.ConnectionStrings[connectionName];
+            DbProviderFactory factory = DbProviderFactories.GetFactory(connStringSettings.ProviderName);
+
+            DbConnection connection = factory.CreateConnection();
+            connection.ConnectionString = connStringSettings.ConnectionString;
+            return connection;
+        }
+    } 
+
+}
